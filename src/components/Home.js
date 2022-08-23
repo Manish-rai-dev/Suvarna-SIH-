41911a00a3231086a6ccf7dd226e8b088ce0dd1c
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-08-24 02:14:57 +0530

    homepage updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import "./Home.css";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useUserAuth } from "../context/UserAuthContext";
-
+import Sidebar from "./Sidebar";
+import ecg from "./images/ecg.svg";
+import report from "./images/report.svg";
+import stomach from "./images/stomach.svg";
 const Home = () => {
   const { logOut, user } = useUserAuth();
   const navigate = useNavigate();
@@ -17,16 +20,30 @@ const Home = () => {
   };
   return (
     <>
-      <div className="p-4 box mt-3 text-center">
+    
+   
+
+<div className="cardcontainer">
+<div className=" text-center">
         Hello Welcome <br />
         {user && user.email}
       </div>
 
 
-    
+    <div className="card1">
+    <h3>Prescription</h3>
+    <img src={stomach} alt="stomach"className="imghome"  />
+    </div>
 
-
-
+<div className="card2">
+<h3>Reports</h3>
+<img src={ecg} alt="ecg " className="imghome" />
+</div>
+<div className="card3">
+<h3>Medical History</h3>
+<img src={report} alt="rport" className="imghome" />
+</div>
+</div>
       <div className="d-grid gap-2">
         <Button variant="primary" onClick={handleLogout}>
           Log out
